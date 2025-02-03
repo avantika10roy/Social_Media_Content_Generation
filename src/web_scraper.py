f05026632576f94752ef68ddd76c8a6e2e7ca336
PRIYAM PAL <priyam@itobuz.com>
2025-02-03 18:01:07 +0530

    bash file added

--- src/web_scraper.py
+++ src/web_scraper.py
@@ -497,19 +497,19 @@ class SocialMediaScraper:
                     image_urls         = list(set([img['src'] for img in valid_images if img.get('src')]))
                     image_paths        = [self.download_image(url, post_id) for url in image_urls if url]
 
-                    post_data          = {"Post_Heading": heading,
-                                          "Post_Content": content if content else "No content",
-                                          "Hashtags": ', '.join(hashtags) if hashtags else "No hashtags",
-                                          "Emojis": ', '.join(emojis) if emojis else "No emojis",
-                                          "Image URLs": ', '.join(image_urls),
-                                          "Image Paths": ', '.join(filter(None, image_paths))
+                    post_data          = {"post_heading": heading,
+                                          "post_content": content if content else "No content",
+                                          "hashtags": ', '.join(hashtags) if hashtags else "No hashtags",
+                                          "emojis": ', '.join(emojis) if emojis else "No emojis",
+                                          "image_URLs": ', '.join(image_urls),
+                                          "image_paths": ', '.join(filter(None, image_paths))
                                           }
 
                     full_post_content   = f"{heading} {content}".lower()
                     is_duplicate        = False
             
                     for existing_post in scraped_data:
-                        existing_content = f"{existing_post['Post_Heading']} {existing_post['Post_Content']}".lower()
+                        existing_content = f"{existing_post['post_heading']} {existing_post['post_content']}".lower()
                         
                         if (full_post_content in existing_content) or (existing_content in full_post_content):
                             is_duplicate = True
