7963f84bdb286ddbfed90951321ecbffd07394a0
PRIYAM PAL <priyam@itobuz.com>
2025-02-04 15:32:29 +0530

    changes

--- setup.sh
+++ setup.sh
@@ -16,6 +16,15 @@ else
     echo "Conda environment 'social_media_content_env' already exists."
 fi
 
+# # Activate the Conda environment
+# echo "Activating Conda environment 'social_media_content_env'..."
+# conda init bash
+# source ~/.bashrc   
+# conda activate social_media_content_env
+
+# # Start a new interactive shell with the activated environment
+# exec bash --rcfile <(echo "source activate social_media_content_env")
+
 # Activate the Conda environment
 echo "Activating Conda environment 'social_media_content_env'..."
 conda activate social_media_content_env
