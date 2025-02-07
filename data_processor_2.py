ddd31770871f6be6e1d4daccc6ec9267d81cd6fd
avantika <avantika@itobuz.com>
2025-02-07 19:11:48 +0530

    data processor added

---
+++ data_processor_2.py
@@ -0,0 +1,13 @@
+import torch
+from src.data_preprocesser.text_preprocessing import TextPreprocessing
+from src.data_preprocesser.image_preprocessing import ImagePreprocessor
+import pandas as pd
+import numpy as np
+import nltk
+from config.config import Config
+
+class DataProcessor:
+
+    """---"""
+
+    def __init__(self,  )
