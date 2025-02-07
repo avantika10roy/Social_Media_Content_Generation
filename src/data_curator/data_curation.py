097342cc3b205fe02de358b31d8b5a252922fdc1
PRIYAM PAL <priyam@itobuz.com>
2025-02-07 18:31:04 +0530

    image_curation function

--- src/data_curator/data_curation.py
+++ src/data_curator/data_curation.py
@@ -1,3 +1,10 @@
+## ----- DONE BY PRIYAM PAL -----
+
+# DEPENDENCIES
+
+import os
+import json
+import shutil
 import pandas as pd
 from ..utils.logger import LoggerSetup
 from ..utils.data_saver import DataSaver
@@ -26,16 +33,16 @@ class DataCuration:
             Logs an error if the JSON file path is invalid.
         """
         try:
-            self.linkedin_cleaned_data_path = linkedin_cleaned_data_path
-            self.facebook_cleaned_data_path = facebook_cleaned_data_path
-            self.instagram_cleaned_data_path = instagram_cleaned_data_path
+            self.linkedin_cleaned_data_path   = linkedin_cleaned_data_path
+            self.facebook_cleaned_data_path   = facebook_cleaned_data_path
+            self.instagram_cleaned_data_path  = instagram_cleaned_data_path
 
             dataCurator_logger.info("Data Curator Class Initialized Successfully")
 
         except Exception as e:
             dataCurator_logger.error(f"Error Occurred in Initializing the Class: {repr(e)}")
 
-    def data_curation(self) -> pd.DataFrame:
+    def text_curation(self) -> pd.DataFrame:
         """
         Reads and merges data from LinkedIn, Instagram, and Facebook cleaned JSON files.
 
@@ -50,23 +57,32 @@ class DataCuration:
         """
     
         try:
-            linked_cleaned_data = DataSaver.data_reader(self.linkedin_cleaned_data_path)
-            facebook_cleaned_data = DataSaver.data_reader(self.facebook_cleaned_data_path)
-            instagram_cleaned_data = DataSaver.data_reader(self.instagram_cleaned_data_path)
+            linked_cleaned_data     = DataSaver.data_reader(self.linkedin_cleaned_data_path)
+            facebook_cleaned_data   = DataSaver.data_reader(self.facebook_cleaned_data_path)
+            instagram_cleaned_data  = DataSaver.data_reader(self.instagram_cleaned_data_path)
 
             dataCurator_logger.info("Data successfully read from all sources")
 
-            dataframes = [df for df in [linked_cleaned_data, facebook_cleaned_data, instagram_cleaned_data] if df is not None]
+            dataframes              = [df for df in 
+                                       [linked_cleaned_data, 
+                                        facebook_cleaned_data, 
+                                        instagram_cleaned_data
+                                        ] 
+                                       if df is not None
+                                       ]
 
-            if not dataframes:  # If all are None, return an empty DataFrame
+            if not dataframes:
                 dataCurator_logger.error("No valid data found. Returning an empty DataFrame.")
+                
                 return pd.DataFrame()
 
-            combined_data = pd.concat(dataframes, ignore_index=True)
+            combined_data           = pd.concat(dataframes, 
+                                                ignore_index = True)
 
             if not combined_data.empty:
                 DataSaver.data_saver(combined_data, Config.CURATED_POST_DATA_PATH)
                 dataCurator_logger.info("Data Curation Completed Successfully")
+            
             else:
                 dataCurator_logger.warning("No valid data to save after merging.")
 
@@ -74,7 +90,65 @@ class DataCuration:
 
         except Exception as e:
             dataCurator_logger.error(f"Error Occurred in data_curation function: {repr(e)}")
+            
             return pd.DataFrame()
+        
+        
+    def image_curation(self, json_path: str, curated_images_dir: str):
+        """
+        Copies images from their original local paths to a curated_images directory 
+        and updates the JSON file with the new image paths.
+
+        Arguments:
+        ----------
+            json_path (str)          : Path to the curated_post_data.json file.
+            curated_images_dir (str) : Directory where images will be copied.
+
+        Returns:
+        --------
+            None
+        """
+        
+        try:
+            os.makedirs(curated_images_dir, exist_ok = True)
+
+            with open(json_path, "r", encoding="utf-8") as f:
+                posts_data            = json.load(f)
+            
+            f.close()
+
+            for post in posts_data:
+                if "image_paths" in post and post["image_paths"]:
+                    
+                    image_paths       = [img.strip() for img in post["image_paths"].split(",")]
+
+                    updated_paths     = []
+                    
+                    for img_path in image_paths:
+                        
+                        if os.path.exists(img_path): 
+                            filename  = os.path.basename(img_path)
+                            new_path  = os.path.join(curated_images_dir, filename)
+
+                            shutil.copy(img_path, new_path)
+                            updated_paths.append(new_path.replace("\\", "/"))
+                            
+                            dataCurator_logger.info("Image Currated Successfully")
+                        
+                        else:
+                            dataCurator_logger.warning(f"Warning: Image not found - {img_path}")
+
+                    post["image_paths"] = ", ".join(updated_paths)
+
+            with open(json_path, "w", encoding = "utf-8") as f:
+                json.dump(posts_data, f, indent = 4, ensure_ascii = False)
+
+            dataCurator_logger.info("Images copied and JSON updated successfully!")
+
+        except Exception as e:
+            dataCurator_logger.error(f"Error in updating image paths: {repr(e)}")
+        
+        
         
        
 # EXAMPLE USAGE 
