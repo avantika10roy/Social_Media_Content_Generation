070d1ed6cf3b817fa71c0bc1921dec347ffdb81f
IT042307 <manu@itobuz.com>
2025-02-12 12:17:13 +0530

    set_global_seed file created

---
+++ src/utils/set_seed.py
@@ -0,0 +1,20 @@
+# -------- Done By Manu Bhaskar ---------
+
+# -------- Dependencies ------------
+from src.utils.logger import LoggerSetup
+# from config.config import RANDOM_SEED
+from transformers import set_seed
+import numpy as np
+import random
+import torch
+import os
+
+def set_global_seed(logger: LoggerSetup ,seed: int = 42) -> None:
+    np.random.seed(seed)
+    random.seed(seed)
+    set_seed(seed=seed)
+    torch.manual_seed(seed)
+    torch.mps.manual_seed(seed)
+    # Set a fixed value for the hash seed
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    logger.info(f"Random seed set as {seed}")
