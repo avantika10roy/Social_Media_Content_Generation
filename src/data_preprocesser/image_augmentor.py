12f138bff1a699e8c03dfcea0ff59da015dae4eb
Jit-itobuz <jit@itobuz.com>
2025-02-18 12:47:58 +0530

    changes made

--- src/data_preprocesser/image_augmentor.py
+++ src/data_preprocesser/image_augmentor.py
@@ -1,9 +1,15 @@
+# ________ DONE BY JIT________
+
 # DEPENDENCIES
 import os
 import cv2
+import numpy as np
 import pandas as pd
 import albumentations as A
+from collections import Counter
 from ..utils.logger import LoggerSetup
+from ..utils.color_themes import get_color_themes
+from sklearn.cluster import MiniBatchKMeans
 
 preprocessor_log = LoggerSetup(logger_name='preprocessor_log', log_filename_prefix='PreProcessor').get_logger()
 class PreProcessor:
@@ -88,6 +94,7 @@ class PreProcessor:
         
         augmented_data = []
 
+        preprocessor_log.info("Augmenting images....")
         try:
             for _, item in df.iterrows():
                 image_path = item["image_path"]
@@ -145,5 +152,93 @@ class PreProcessor:
 
         preprocessor_log.info(f"Augmentation completed.")
         return pd.DataFrame(augmented_data)
+    
 
+    def extract_color(self, image_path):
+        '''Extracts dominant colors using clustering.'''
+        try:
+            image           = cv2.imread(image_path)
+            image           = cv2.resize(image, (100, 100))
+            image           = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            pixels          = image.reshape(-1, 3)
+
+            kmeans          = MiniBatchKMeans(n_clusters=3, random_state=42)
+            kmeans.fit(pixels)
+            dominant_color  = kmeans.cluster_centers_[np.argmax(np.bincount(kmeans.labels_))].astype(int)
+
+            color_dict      = get_color_themes()
+            color_array     = np.array(list(color_dict.values()))
+            distances       = np.linalg.norm(color_array - dominant_color, axis=1)
+            closest_colors  = np.array(list(color_dict.keys()))[np.argsort(distances)[:2]]
+
+            return f"{closest_colors[0]}-{closest_colors[1]}"
+        
+        except Exception as e:
+            preprocessor_log.error(f"Error extracting color from {image_path}: {repr(e)}")
+            return "unknown"
+
+    def get_dominant_colors(self, df: pd.DataFrame):
+        """
+        Extract dominant colors for each image in the DataFrame and add a 'color_theme' column.
+        """
+        preprocessor_log.info("Extracting dominant colors.")
+        df["color_theme"] = df["image_path"].apply(self.extract_color)
+        preprocessor_log.info("Successfully extracted dominant colors.")
+        return df
+    
+    def determine_layout(self, image_path):
+        """
+            Function to check layout of images
+
+            Arguments:
+            ----------
+            - image : path to input images
+        """
+        try:
+            image = cv2.imread(image_path)
+            gray        = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+            _, thresh   = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY_INV)
+
+            # Determine Contour
+            contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            # Calculate whether layout is above or below threshold
+            text_area   = sum(cv2.contourArea(cnt) for cnt in contours)
+            img_area    = image.shape[0] * image.shape[1]
+
+            return "text-heavy" if text_area / img_area > 0.5 else "minimalist"
+        except Exception as e:
+            preprocessor_log.error(f"Error determining layout for {image_path}: {repr(e)}")
+            return "unknown"
+    
+    def get_layout(self, df):
+       ''' Extracts layout of the image.'''
+       preprocessor_log.info("Determining Layout.")
+       df['image_layout'] =  df['image_path'].apply(self.determine_layout)
+       preprocessor_log.info('Image layout determined.')
+       return df
+    
+
+    def run_preprocessor(self, df: pd.DataFrame, save_dir: str):
+        '''
+        Runs the data preprocessor.
+        Arguments:
+        ---------
+        df : Input Data.
+        save_dir : Directory to save augmented images.
+
+        Returns:
+        --------
+        df : Preprocessed data.
+        '''
+        try:
+            df = self.convert_to_separate_rows(df)
+            df = self.image_augmentation(df, save_dir)
+            df = self.get_dominant_colors(df)
+            df = self.get_layout(df)
+            preprocessor_log.info("Preprocessing completed successfully.")
+            return df
+        except Exception as e:
+            preprocessor_log.error(f"Error in run_all: {repr(e)}")
+            return df
 
