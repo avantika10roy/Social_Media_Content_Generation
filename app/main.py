76f85d2a9f60a550a05cc77cc847768513308638
Amrit-Itobuz <amrit@itobuz.com>
2025-02-21 11:02:39 +0530

    commited by amrit

--- app/main.py
+++ app/main.py
@@ -1,12 +1,37 @@
 from fastapi import FastAPI
-from fastapi import HTTPException
+from src.prompts import llm_prompt
+from pydantic import BaseModel
+
 
 app = FastAPI()
+class Prompt(BaseModel):
+    company_name      : str
+    platform          : str
+    topic             : str
+    extra_details     : str
+    occasion          : str
+
+
 
 @app.get("/")
-def opening():
-    return {"message":"Welcome to Social Media Content Genaretor XD"}
+def start():
+    return {"Meaasge":"Welcome to LLMs"}
 
 @app.post("/llm")
-def llm_op():
-    return {"message":"LLM Started"}
+def gen_prompt(prompt : Prompt):
+    company_name     = prompt.company_name
+    platform         = prompt.platform
+    topic            = prompt.topic
+    extra_details    = prompt.extra_details
+    occation         = prompt.occasion
+
+
+    prompt = llm_prompt(platform        = platform,
+                        topic           = topic,
+                        company_name   = company_name,
+                        extra_details  = extra_details,
+                        occasion       = occation )
+    return {"Generated Prompt ":prompt}
+    
+
+
