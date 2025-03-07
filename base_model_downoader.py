ca55bcdf79a84d7701676e59eeafe482da6c1d08
satyaki-itobuz <satyaki@itobuz.com>
2025-03-07 11:03:36 +0530

    Delete base_model_downoader.py

--- base_model_downoader.py
+++
@@ -1,9 +0,0 @@
-from huggingface_hub import snapshot_download
-
-# Define model repository name
-model_name = "stabilityai/stable-diffusion-xl-base-1.0"
-
-# Download the model (this may take some time)
-model_path = snapshot_download(repo_id=model_name, local_dir="sdxl_base_1_0")
-
-print(f"Model downloaded to: {model_path}")
